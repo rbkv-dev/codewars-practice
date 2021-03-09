01f653f892c279a7d5c914f90d0243f95964ca07
Nikita Rebrikov <rbkv.dev@gmail.com>
2021-03-09 15:51:50 +0200

    6kyu Roman Numerals Encoder

---
+++ 6kyu Roman Numerals Encoder.js
@@ -0,0 +1,28 @@
+const ROMAN_NUMBERS = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+function solution(num) {
+  let res = "";
+  for (i in ROMAN_NUMBERS) {
+    while (num >= ROMAN_NUMBERS[i]) {
+      res += i;
+      num -= ROMAN_NUMBERS[i];
+    }
+  }
+  return res;
+}
+
+module.exports = solution;
--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const decodeMorse = require("./6kyu Decode the Morse code (part1)");
-console.log(decodeMorse(".... . -.--   .--- ..- -.. ."));
+// const {
+//   decodeMorse,
+//   decodeBits,
+// } = require("./4kyu Decode the Morse code, advanced (part2)");
+// // const decodeMorse = require("./6kyu Decode the Morse code (part1)");
+// console.log(decodeMorse(".... . -.--   .--- ..- -.. ."));
+// console.log(
+//   decodeBits(
+//     "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
+//   )
+// );
+// console.log("···· · −·−−   ·−−− ··− −·· ·");
+
+const solution = require("./6kyu Roman Numerals Encoder");
+
+console.log("res1", solution(2));
+console.log("res3", solution(122));
